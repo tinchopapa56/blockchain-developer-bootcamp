d4ef1563f99cba455b9d7f20e41ce724c2206e7d
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2024-02-22 20:22:26 -0300

    Make and cancell orders complete

--- smartContracts/contracts/Exchange.sol
+++ smartContracts/contracts/Exchange.sol
@@ -8,6 +8,22 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => Order) public orders;
+    mapping(uint256 => bool) public ordersCancelled;
+    mapping(address => Order) public ordersOf;
+    uint256 public ordersCount;
+
+    struct Order {
+        uint256 id;
+        address user;
+
+        address tokenToBuy;
+        address tokenToSell;
+        uint256 amountToBuy;
+        uint256 amountToSell;
+
+        uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -16,10 +32,16 @@ contract Exchange {
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdrawal(address token, address user, uint256 amount, uint256 balance);
+    event CreatedOrder(
+        uint256 id, address user,address tokenToBuy, address tokenToSell,uint256 amountToBuy, uint256 amountToSell,uint256 timestamp
+    );
+    event CancelledOrder(
+        uint256 id, address user,address tokenToBuy, address tokenToSell,uint256 amountToBuy, uint256 amountToSell,uint256 timestamp
+    );
 
     function withdrawToken(address _token, uint256 _amount) public {
 
-        require(tokens[_token][msg.sender] >= _amount, "insufficient balance");
+        require(tokens[_token][msg.sender] >= _amount, "insufficient balance for witdhrawal");
         //transfer tokens to user
         Token(_token).transfer(msg.sender, _amount);
         //update user balance
@@ -45,18 +67,36 @@ contract Exchange {
         emit Deposit(_token, msg.sender, _amount, newBalance);
     }
 
-    function balanceOf(
-        address _token,
-        address _user
-    ) public view returns (uint256) {
+    function balanceOf(address _token, address _user) public view returns (uint256) {
         return tokens[_token][_user];
     }
-    /////////////////////////////////////
 
-    //make orders
-    //cancel
-    //fill orders
+    function makeOrder(address _tokenToBuy, address _tokenToSell, uint256 _amountToBuy, uint256 _amountToSell) 
+        public returns (bool) 
+    {
+        bool hasEnoughBalance = balanceOf(_tokenToSell,msg.sender) >= _amountToSell;
+        require(hasEnoughBalance, "You have Insufficient balance for making order");
 
-    //charge fees
-    //track fee account
+        ordersCount += 1;
+        orders[ordersCount] = Order(
+            ordersCount, msg.sender, _tokenToBuy, _tokenToSell, _amountToBuy, _amountToSell, block.timestamp
+        );
+        emit CreatedOrder(ordersCount, msg.sender, _tokenToBuy, _tokenToSell, _amountToBuy, _amountToSell, block.timestamp);
+        return true;
+    }
+
+    function cancelOrder(uint256 _orderId) public {
+        Order storage order = orders[_orderId];
+
+        bool orderExists = order.id == _orderId;
+        require(orderExists, "order id doesnt exist");
+
+        bool isOrderCreator = address(order.user) == msg.sender;
+        require(isOrderCreator, "only order creator can cancel it");
+
+        ordersCancelled[_orderId] = true;
+
+        emit CancelledOrder(order.id, msg.sender, order.tokenToBuy, order.tokenToSell, order.amountToBuy, order.amountToSell, block.timestamp);
+
+    }
 }
--- smartContracts/test/Exchange.js
+++ smartContracts/test/Exchange.js
@@ -6,19 +6,20 @@ const tokens = (n) => {
 }
 
 describe('Exchange', () => {
-    let deployer, feeAccount, exchange, token1, token2;
+    let deployer, feeAccount, exchange, token1, token2, user2, user1;
     const feePercent = 10
 
     beforeEach(async () => {
         const Exchange = await ethers.getContractFactory("Exchange")
         const Token = await ethers.getContractFactory("Token")
         token1 = await Token.deploy("Dapp University", "DAPP", "1000000")
-        // token2 = await Token.deploy("Dapp University s2", "DAPP2", "2000000")
+        token2 = await Token.deploy("Dai", "MDAI", "2000000")
 
         accounts = await ethers.getSigners()
         deployer = accounts[0]
         feeAccount = accounts[1]
         user1 = accounts[2]
+        user2 = accounts[3]
 
         //trasnfer tokens to user =>
         let tx = await token1.connect(deployer).transfer(user1.address, tokens(100))
@@ -26,7 +27,6 @@ describe('Exchange', () => {
 
         exchange = await Exchange.deploy(feeAccount.address, feePercent)
     })
-
     describe('Deployment', () => {
 
         it("tracks fee account", async () => {
@@ -36,7 +36,6 @@ describe('Exchange', () => {
             expect(await exchange.feePercent()).to.equal(feePercent)
         })
     })
-
     describe("Deposit Tokens", () => {
         let tx, res, amount = tokens(100);
 
@@ -77,8 +76,7 @@ describe('Exchange', () => {
             })
         })
     })
-
-    describe(" WITHDRAW f(x)", () => {
+    describe("WITHDRAW f(x)", () => {
         let tx, res, amount = tokens(100);
 
         describe("Success", () => {
@@ -116,11 +114,11 @@ describe('Exchange', () => {
         describe("Failure", () => {
             it("fails for insufficient balance ", async () => {
                 const tx_to_fail = exchange.connect(user1).withdrawToken(token1.address, amount)
-                await expect(tx_to_fail).to.be.revertedWith("insufficient balance");
+                await expect(tx_to_fail).to.be.revertedWith("insufficient balance for witdhrawal");
             })
         })
     })
-    describe(" Balance of f(X)", () => {
+    describe("Balance of f(X)", () => {
         it("returns user balanceracks the token deposit", async () => {
             let tx, res, amount = tokens(1);
             //approve 
@@ -134,4 +132,163 @@ describe('Exchange', () => {
         })
 
     })
+
+
+    describe("Make Order", () => {
+        let tx, res, amount = tokens(100);
+        let buyAmount = tokens(20)
+        let sellAmount = tokens(1)
+
+        describe("Success", () => {
+
+            beforeEach(async () => {
+                //approve 
+                tx = await token1.connect(user1).approve(exchange.address, amount)
+                res = await tx.wait()
+                //depost
+                tx = await exchange.connect(user1).depositToken(token1.address, amount)
+                res = await tx.wait()
+
+                //MAKE ORDER 
+                tx = await exchange.connect(user1).makeOrder(token2.address, token1.address, buyAmount, sellAmount)
+                res = await tx.wait()
+            })
+
+            it("Tracks newly created order", async () => {
+                expect(await exchange.ordersCount()).to.equal(1)
+                expect(await exchange.ordersCount()).not.to.equal(2)
+
+            })
+            it("emits CreatedOrder event", async () => {
+                const event = res.events[0]
+
+                expect(event.event).to.equal("CreatedOrder")
+
+                const args = event.args
+                expect(args.id).to.equal(1)
+                expect(args.user).to.equal(user1.address)
+                expect(args.tokenToBuy).to.equal(token2.address)
+                expect(args.tokenToSell).to.equal(token1.address)
+                expect(args.amountToBuy).to.equal(buyAmount)
+                expect(args.amountToSell).to.equal(sellAmount)
+                expect(args.timestamp).to.at.least(1)
+            })
+        })
+        describe("Failure", async () => {
+
+            it("REJECTS, not enough balance", async () => {
+                await expect(exchange.connect(user1).makeOrder(token1.address, token2.address, tokens(10000), tokens(150000)))
+                    .to.be.revertedWith('You have Insufficient balance for making order')
+            })
+
+        })
+    })
+
+    describe("Order Actions", () => {
+
+        let transaction, result
+        let amount = tokens(1)
+    
+        beforeEach(async () => {
+          // user1 deposits tokens
+          transaction = await token1.connect(user1).approve(exchange.address, amount)
+          result = await transaction.wait()
+    
+          transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+          result = await transaction.wait()
+    
+          // Make an order
+          transaction = await exchange.connect(user1).makeOrder(token2.address, token1.address, amount, amount)
+          result = await transaction.wait()
+        })
+
+        describe("Cancel order", async () => {
+            describe("Success", () => {
+                beforeEach(async () => {
+                    //ACTUALLY CANCEL_ORDER
+                    tx = await exchange.connect(user1).cancelOrder(1)
+                    res = await tx.wait()
+                })
+                it("updatesCancelledOrders", async () => {
+                    expect(await exchange.ordersCancelled(1)).to.equal(true)
+                })
+                it("emits CancelledOrder event", async () => {
+                    const event = res.events[0]
+
+                    expect(event.event).to.equal("CancelledOrder")
+
+                    const args = event.args
+                    expect(args.id).to.equal(1)
+                    expect(args.user).to.equal(user1.address)
+                    expect(args.tokenToBuy).to.equal(token2.address)
+                    expect(args.tokenToSell).to.equal(token1.address)
+                    expect(args.amountToBuy).to.equal(tokens(1)) //amount
+                    expect(args.amountToSell).to.equal(tokens(1)) //(amount)
+                    expect(args.timestamp).to.at.least(1)
+                })
+            })
+            describe("Failure", async () => {
+
+                beforeEach(async () => {
+                    // user1 deposits tokens
+                    transaction = await token1.connect(user1).approve(exchange.address, amount)
+                    result = await transaction.wait()
+                    transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+                    result = await transaction.wait()
+                    // Make an order
+                    transaction = await exchange.connect(user1).makeOrder(token2.address, token1.address, amount, amount)
+                    result = await transaction.wait()
+                  })
+          
+                  it('rejects invalid order ids', async () => {
+                    const invalidOrderId = 99999
+                    await expect(exchange.connect(user1).cancelOrder(invalidOrderId)).to.be.revertedWith("order id doesnt exist")
+                  })
+          
+                  it('rejects unauthorized cancelations', async () => {
+                    await expect(exchange.connect(user2).cancelOrder(1)).to.be.revertedWith("only order creator can cancel it")
+                  })
+
+            })
+        })
+
+        describe("Fill order", async () => {
+            describe("Success", () => {
+
+            })
+            describe("Failure", async () => {
+
+
+            })
+
+        })
+
+    })
 })
+
+
+// describe('Success', async () => {
+//     beforeEach(async () => {
+//       transaction = await exchange.connect(user1).cancelOrder(1)
+//       result = await transaction.wait()
+//     })
+
+//     it('updates canceled orders', async () => {
+//       expect(await exchange.orderCancelled(1)).to.equal(true)
+//     })
+
+//     it('emits a Cancel event', async () => {
+//       const event = result.events[0]
+//       expect(event.event).to.equal('Cancel')
+
+//       const args = event.args
+//       expect(args.id).to.equal(1)
+//       expect(args.user).to.equal(user1.address)
+//       expect(args.tokenGet).to.equal(token2.address)
+//       expect(args.amountGet).to.equal(tokens(1))
+//       expect(args.tokenGive).to.equal(token1.address)
+//       expect(args.amountGive).to.equal(tokens(1))
+//       expect(args.timestamp).to.at.least(1)
+//     })
+
+//   })
